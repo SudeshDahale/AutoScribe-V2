79e7d8f80f4a50d50a16eec21cb3be23b56995b7
Sudesh Dahale <sudeshdahale1008@gmail.com>
2026-08-12 18:43:50 +0530

    Update backend/app/models/module.py

---
+++ backend/app/models/module.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.session import Base
+
+
+class Module(Base):
+    __tablename__ = "modules"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    repository_id: Mapped[int] = mapped_column(ForeignKey("repositories.id"))
+    name: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    icon: Mapped[str] = mapped_column(String)
